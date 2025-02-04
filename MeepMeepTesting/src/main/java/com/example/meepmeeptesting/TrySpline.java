fc959ebd09da852a65d4feda74cf094860608002
Copper <CopperCookiesftc@gmail.com>
2025-02-03 21:28:58 -0600

    MeepMeep with pushing last block

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TrySpline.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TrySpline.java
@@ -14,16 +14,18 @@ public class TrySpline {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         //Code goes in here
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(14, -61.5, Math.toRadians(180)))
 //drop specimen 1
-                        .waitSeconds(4)
-                        .strafeToConstantHeading(new Vector2d(15,-31.5))
+                        .waitSeconds(5)
+                        .strafeToConstantHeading(new Vector2d(9,-31.5))
                         .waitSeconds(1)
-                        .strafeToConstantHeading(new Vector2d(15,-32))
+
+
+                        .strafeToConstantHeading(new Vector2d(9,-35))
 
 
 
@@ -44,36 +46,34 @@ public class TrySpline {
                         .strafeTo(new Vector2d(48.2, -5))
                         .splineToConstantHeading(new Vector2d(58, -5), Math.toRadians(-90))
                         .splineToConstantHeading(new Vector2d(58, -57), Math.toRadians(270.00))
-//
-//                        //get away from 2nd pushed block and change heading
-//
-//                        //pick up specimen 1
-//                        .strafeToLinearHeading(new Vector2d(45,-45),Math.toRadians(360))
-//                        .strafeToConstantHeading(new Vector2d(31.3,-58.2))
-//                        .waitSeconds(0.7)
-//
-//
-//                        //drop specimen 2 and come back
-//                        .strafeToLinearHeading(new Vector2d(4, -30.6),Math.toRadians(180))
-//                        .waitSeconds(1)
-//                        .strafeToLinearHeading(new Vector2d(31.6, -54),Math.toRadians(360))
-//                        .strafeTo(new Vector2d(31.6,-57.6))
-//                        .waitSeconds(0.7)
-//
-//
-//                        //drop specimen 3 and come back
-//                        .strafeToLinearHeading(new Vector2d(6, -30.6),Math.toRadians(180))
-//                        .waitSeconds(1)
-//
-//                        .strafeToLinearHeading(new Vector2d(31.6, -54),Math.toRadians(360))
-//                        .strafeTo(new Vector2d(31.6,-57.6))
-//                        .waitSeconds(0.7)
-//
-//
-//                        //drop specimen 4 and park
-//                        .strafeToLinearHeading(new Vector2d(8, -30.6),Math.toRadians(180))
-//                        .waitSeconds(1)
-//                        .strafeToLinearHeading(new Vector2d(47,-56),Math.toRadians(90), new TranslationalVelConstraint(100))
+
+                        .waitSeconds(0.8)
+
+
+                        //drop specimen 2 and come back
+                        .strafeToLinearHeading(new Vector2d(4, -36),Math.toRadians(180))
+                        .strafeToConstantHeading(new Vector2d(4,-30.6))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(31.6, -54),Math.toRadians(360))
+                        .strafeTo(new Vector2d(31.6,-57.6))
+                        .waitSeconds(0.7)
+
+
+                        //drop specimen 3 and come back
+                        .strafeToLinearHeading(new Vector2d(6, -30.6),Math.toRadians(180))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(31.6, -54),Math.toRadians(360))
+                        .strafeTo(new Vector2d(31.6,-57.6))
+                        .waitSeconds(0.7)
+
+                        .strafeToLinearHeading(new Vector2d(9, -30.6),Math.toRadians(180))
+                        .waitSeconds(0.7)
+
+                        .strafeTo(new Vector2d(9,-35))
+
+                        .splineToConstantHeading(new Vector2d(40, -28), Math.toRadians(90.00))
+                        .splineToConstantHeading(new Vector2d(61.4, -10), Math.toRadians(0.00))
+                        .strafeTo(new Vector2d(61.4,-51))
 
                         .build()
         );
