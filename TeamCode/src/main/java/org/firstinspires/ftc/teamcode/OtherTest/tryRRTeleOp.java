3169d29b65dae0185e3cbadd48f37a4c31893df0
Copper <CopperCookiesftc@gmail.com>
2025-02-08 07:59:18 -0600

    RRTeleOp correction 1 2/8 8:00 a.m.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OtherTest/tryRRTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OtherTest/tryRRTeleOp.java
@@ -12,70 +12,61 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Drawing;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-@TeleOp(name = "FieldCentricTeleOp", group = "TeleOp")
+@TeleOp(name = "tryRRTeleOp", group = "TeleOp")
 public class tryRRTeleOp extends OpMode {
 
     private MecanumDrive drive;
 
     @Override
     public void init() {
-        //Fix beginPose
-        Pose2d beginPose = new Pose2d(9, -61.5, Math.toRadians(90));
+        Pose2d beginPose = new Pose2d(47, -54, Math.toRadians(90));
         drive = new MecanumDrive(hardwareMap, beginPose);
         drive.localizer.setPose(beginPose);
-
     }
 
     @Override
     public void loop() {
         RRDrive();
+
+        telemetry.update();
     }
 
     private void RRDrive() {
         Pose2d pose = drive.localizer.getPose();
-        double heading = pose.heading.toDouble(); // Get the robot's heading in radians
+        double heading = pose.heading.toDouble();
 
         double vertical = -gamepad2.left_stick_y;
         double strafe = -gamepad2.left_stick_x;
+        double brake = 1.0 - (0.6 * gamepad2.right_trigger);
 
-        double adjustedVertical = vertical * Math.sin(heading) + strafe * Math.cos(heading);
-        double adjustedStrafe = vertical * Math.cos(heading) - strafe * Math.sin(heading);
+        double adjustedVertical = vertical * Math.cos(heading) - strafe * Math.sin(heading);
+        double adjustedStrafe = vertical * Math.sin(heading) + strafe * Math.cos(heading);
 
-//        double adjustedVertical = vertical * Math.cos(heading) - strafe * Math.sin(heading);
-//        double adjustedStrafe = vertical * Math.sin(heading) + strafe * Math.cos(heading);
-        Vector2d positionInput = new Vector2d(adjustedStrafe, adjustedVertical);
+        Vector2d positionInput = new Vector2d(adjustedStrafe, adjustedVertical).times(brake);
+        double turn = -gamepad2.right_stick_x * brake;
 
-        drive.setDrivePowers(new PoseVelocity2d(
-                positionInput,
-                //turn
-                -gamepad2.right_stick_x
-        ));
+        drive.setDrivePowers(new PoseVelocity2d(positionInput, turn));
         drive.updatePoseEstimate();
 
-
         if (gamepad2.b) {
-            Actions.runBlocking(
-                    drive.actionBuilder(pose)
-                            .strafeToLinearHeading(new Vector2d(40,-30),Math.toRadians(360))
-                            .build());
-        } else if (gamepad2.x) {
-            Actions.runBlocking(
-                    drive.actionBuilder(pose)
-                            .endTrajectory()
-                            .build()
-            );
+            Actions.runBlocking(drive.actionBuilder(pose)
+                    .strafeToLinearHeading(new Vector2d(28, -55),Math.toRadians(360))
+                    .build());
+        }
+
+        if (gamepad2.x) {
+            Actions.runBlocking(drive.actionBuilder(pose)
+                    .endTrajectory()
+                    .build());
         }
 
         telemetry.addData("x", pose.position.x);
         telemetry.addData("y", pose.position.y);
         telemetry.addData("heading (deg)", Math.toDegrees(heading));
-        telemetry.update();
 
         TelemetryPacket packet = new TelemetryPacket();
         packet.fieldOverlay().setStroke("#3F51B5");
         Drawing.drawRobot(packet.fieldOverlay(), pose);
         FtcDashboard.getInstance().sendTelemetryPacket(packet);
-
-
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTestFiles/RedBackTry4Spec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTestFiles/RedBackTry4Spec.java
@@ -6,11 +6,13 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.AccelConstraint;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.AngularVelConstraint;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.VelConstraint;
@@ -116,7 +118,6 @@ public class RedBackTry4Spec extends LinearOpMode {
         Action park = drive.actionBuilder(drive.localizer.getPose())
                 .strafeToLinearHeading(new Vector2d(47,-56),Math.toRadians(90), new TranslationalVelConstraint(90), baseAccelConstraint)
                 .build();
-
 
 
 
